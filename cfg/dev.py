1e444d6c1424e5f13d5d16a5a4923f93a084dc5f
Robert Nguyen <robert@gotitapp.co>
2019-05-23 15:20:04 +0700

    Configuring database for dev environment

--- cfg/dev.py
+++ cfg/dev.py
@@ -2,7 +2,7 @@ from .base import _Config
 
 
 class _DevConfig(_Config):
-    SQL_ALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://robert:robert@localhost/catalog_data'
+    SQL_ALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://robert:robert@localhost/catalog_restful_api'
     DEBUG = True
 
 
